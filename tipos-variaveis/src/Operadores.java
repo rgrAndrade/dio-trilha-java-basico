bed7960952448e5fe3676e3967d88803260a8e85
Rogério Andrade <bestdev.top@gmail.com>
2024-05-03 11:45:42 -0300

    iniciando os estudos

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,22 @@
+
+import javax.lang.model.util.ElementScanner14;
+
+public class Operadores {
+    public static void main ( String[] args) {
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        System.out.println("numero1 e IGUAL numero2? " +simNao);
+
+
+
+       }
+}
+
+
+
+
+
+
