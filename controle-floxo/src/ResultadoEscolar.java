c12ef144e993008e10d749a2d8a74d27e4bf12cf
Rogério Andrade <bestdev.top@gmail.com>
2024-05-05 11:06:41 -0300

    melhorando as condições de resultado escolar

--- controle-floxo/src/ResultadoEscolar.java
+++ controle-floxo/src/ResultadoEscolar.java
@@ -1,11 +1,17 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 6;
+        int nota = 5;
        
        if(nota >= 7)
         System.out.println("Aprovado");
+
+       else if( nota >= 5 && nota < 7)
+        System.out.println("Recuperação");
+          
 
        else
         System.out.println("Reprovado");
     }
 }
+
+
