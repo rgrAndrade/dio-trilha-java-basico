11d4b924a03269b77b1449b39750ea150d2be035
Rogério Andrade <bestdev.top@gmail.com>
2024-05-05 12:21:43 -0300

    Criação da Classe Exemplo do Uso do For

---
+++ controle-floxo/src/ExemploFor.java
@@ -0,0 +1,7 @@
+public class ExemploFor {
+    public static void main(String[]args){
+        for(int caneirinhos = 1; caneirinhos <= 7; caneirinhos ++){
+            System.out.println("Pegando no SONO");
+        }
+    }
+}
