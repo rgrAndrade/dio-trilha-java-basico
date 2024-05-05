4f55f2bf3a59f8bf1e3920ba6c6e44f22b2b49d3
Rogério Andrade <bestdev.top@gmail.com>
2024-05-05 11:28:52 -0300

    melhorando a refatoração da classe Resultado Escolar

--- controle-floxo/src/ResultadoEscolar.java
+++ controle-floxo/src/ResultadoEscolar.java
@@ -14,7 +14,8 @@ public class ResultadoEscolar {
     //
 
         int nota = 5;
-        String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+        //String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+        String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
         System.out.println(resultado);
 
      }
