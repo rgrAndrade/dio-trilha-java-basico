3e8a870fbfa323fd555d865fdf38791fb20c1c3c
Rogério Andrade <bestdev.top@gmail.com>
2024-05-05 11:26:39 -0300

    Refatoração da Classe Resultado Escolar

--- controle-floxo/src/ResultadoEscolar.java
+++ controle-floxo/src/ResultadoEscolar.java
@@ -1,17 +1,23 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 5;
+    //     int nota = 5;
        
-       if(nota >= 7)
-        System.out.println("Aprovado");
+    //    if(nota >= 7)
+    //     System.out.println("Aprovado");
 
-       else if( nota >= 5 && nota < 7)
-        System.out.println("Recuperação");
+    //    else if( nota >= 5 && nota < 7)
+    //     System.out.println("Recuperação");
           
 
-       else
-        System.out.println("Reprovado");
-    }
+    //    else
+    //     System.out.println("Reprovado");
+    //
+
+        int nota = 5;
+        String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado);
+
+     }
 }
 
 
