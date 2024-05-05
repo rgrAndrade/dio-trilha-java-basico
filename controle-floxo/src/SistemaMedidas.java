cb5415b034280b4e85a8bee091c09adb271517dc
Rogério Andrade <bestdev.top@gmail.com>
2024-05-05 11:41:13 -0300

    Refatoração da Classe Sistema de Medidas

--- controle-floxo/src/SistemaMedidas.java
+++ controle-floxo/src/SistemaMedidas.java
@@ -1,14 +1,34 @@
 public class SistemaMedidas {
     public static void main(String[] args) {
+        // String sigla = "M";
+
+		// if(sigla == "P")
+		// 	System.out.println("PEQUENO");
+		// else if(sigla == "M")
+		// 	System.out.println("MEDIO");
+		// else if(sigla == "G")
+		// 	System.out.println("GRANDE");
+		// else
+		// 	System.out.println("INDEFINIDO");
+
+
         String sigla = "M";
 
-		if(sigla == "P")
+		switch (sigla) {
+		case "P":{
 			System.out.println("PEQUENO");
-		else if(sigla == "M")
+			break;
+		}
+		case "M":{
 			System.out.println("MEDIO");
-		else if(sigla == "G")
+			break;
+		}
+		case "G":{
 			System.out.println("GRANDE");
-		else
+			break;
+		}
+		default:
 			System.out.println("INDEFINIDO");
+		}
     }
 }
