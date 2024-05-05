a1d90cddb41e316d6ec876e54d6ab8ae43bc5846
Rogério Andrade <bestdev.top@gmail.com>
2024-05-05 12:31:27 -0300

    Criação da Classe Exemplo do Uso de For com Arrays

--- controle-floxo/src/ExemploFor.java
+++ controle-floxo/src/ExemploFor.java
@@ -1,7 +1,9 @@
 public class ExemploFor {
     public static void main(String[]args){
         for(int caneirinhos = 1; caneirinhos <= 7; caneirinhos ++){
-            System.out.println("Pegando no SONO");
+            System.out.println("Pegando no SONO "+ caneirinhos);
         }
+
+        System.out.println("Joaozinho AGORA dormiu");
     }
 }
