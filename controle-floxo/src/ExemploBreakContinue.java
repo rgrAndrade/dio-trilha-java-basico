9059420a1f8f574a5a3631e41ba03108610c0959
Rogério Andrade <bestdev.top@gmail.com>
2024-05-05 16:43:48 -0300

    Criação da Classe Exemplo do uso do While

--- controle-floxo/src/ExemploBreakContinue.java
+++ controle-floxo/src/ExemploBreakContinue.java
@@ -3,7 +3,7 @@ public class ExemploBreakContinue {
 	
         for(int numero = 1; numero <=5; numero ++){
             if(numero==3)
-                break;
+                continue;
             
             System.out.println(numero);
             
